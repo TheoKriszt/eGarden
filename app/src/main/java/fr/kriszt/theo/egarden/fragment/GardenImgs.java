49206eb13314fd4630fe4e405ecf1cfbfa213726
Michael O <michael.ohayon-ordaz@etu.umontpellier.fr>
2018-05-07 03:49:03 +0200

    Affichage d'historique visuel du jardin ok!

---
+++ app/src/main/java/fr/kriszt/theo/egarden/fragment/GardenImgs.java
@@ -0,0 +1,182 @@
+package fr.kriszt.theo.egarden.fragment;
+//import android.app.Fragment;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.os.Bundle;
+import org.json.JSONArray;
+import java.util.ArrayList;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fr.kriszt.theo.egarden.R;
+import fr.kriszt.theo.egarden.utils.Gallery.GardenAdapter;
+import fr.kriszt.theo.egarden.utils.Gallery.GardenRecyclerAdapter;
+
+public class GardenImgs extends Fragment {
+
+    private static final String TAG = "GardenImgs";
+    List<GardenAdapter> ListOfdataAdapter;
+
+    RecyclerView recyclerView;
+
+    String HTTP_JSON_URL = "http://sparklife.freeboxos.fr:1880/garden/imgs";
+
+    String Image_Name_JSON = "image_title";
+
+    String Image_URL_JSON = "image_url";
+
+    JsonArrayRequest RequestOfJSonArray ;
+
+    RequestQueue requestQueue ;
+
+    //View view ;
+
+    int RecyclerViewItemPosition ;
+
+    RecyclerView.LayoutManager layoutManagerOfrecyclerView;
+
+    RecyclerView.Adapter recyclerViewadapter;
+
+    ArrayList<String> ImageTitleNameArrayListForClick;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.garden_details , container, false);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
+    {
+        ImageView goute = view.findViewById(R.id.auto_watering_icon);
+        goute.setVisibility(View.INVISIBLE);
+
+        ImageTitleNameArrayListForClick = new ArrayList<>();
+
+        ListOfdataAdapter = new ArrayList<>();
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.gardenImgs
+        );
+
+        recyclerView.setHasFixedSize(true);
+
+        final Context mContext = view.getContext();
+        layoutManagerOfrecyclerView = new LinearLayoutManager(view.getContext());
+
+        recyclerView.setLayoutManager(layoutManagerOfrecyclerView);
+
+        JSON_HTTP_CALL();
+
+        // Implementing Click Listener on RecyclerView.
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener()
+        {
+
+            GestureDetector gestureDetector = new GestureDetector(mContext, new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent motionEvent) {
+
+                    return true;
+                }
+
+            });
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+                View view = Recyclerview.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+
+                if(view != null && gestureDetector.onTouchEvent(motionEvent)) {
+
+                    //Getting RecyclerView Clicked Item value.
+                    RecyclerViewItemPosition = Recyclerview.getChildAdapterPosition(view);
+
+                    // Showing RecyclerView Clicked Item value using Toast.
+                    Toast.makeText(view.getContext(), ImageTitleNameArrayListForClick.get(RecyclerViewItemPosition), Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
+
+    }
+
+    public void JSON_HTTP_CALL(){
+
+        RequestOfJSonArray = new JsonArrayRequest(HTTP_JSON_URL,
+
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        ParseJSonResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        requestQueue = Volley.newRequestQueue(this.getView().getContext());
+
+        requestQueue.add(RequestOfJSonArray);
+    }
+
+    public void ParseJSonResponse(JSONArray array){
+
+        for(int i = 0; i<array.length(); i++) {
+
+            GardenAdapter GetDataAdapter2 = new GardenAdapter();
+
+            JSONObject json = null;
+            try {
+
+                json = array.getJSONObject(i);
+
+                GetDataAdapter2.setImageTitle(json.getString(Image_Name_JSON));
+
+                // Adding image title name in array to display on RecyclerView click event.
+                ImageTitleNameArrayListForClick.add(json.getString(Image_Name_JSON));
+
+                GetDataAdapter2.setImageUrl(json.getString(Image_URL_JSON));
+
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+            }
+            ListOfdataAdapter.add(GetDataAdapter2);
+        }
+
+        recyclerViewadapter = new GardenRecyclerAdapter(ListOfdataAdapter, getView().getContext());
+
+        recyclerView.setAdapter(recyclerViewadapter);
+    }
+}
---
+++ app/src/main/java/fr/kriszt/theo/egarden/utils/Gallery/GardenRecyclerAdapter.java
@@ -0,0 +1,109 @@
+package fr.kriszt.theo.egarden.utils.Gallery;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.android.volley.toolbox.NetworkImageView;
+
+
+import java.util.List;
+import com.android.volley.toolbox.ImageLoader;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+
+import fr.kriszt.theo.egarden.R;
+
+public class GardenRecyclerAdapter extends RecyclerView.Adapter<GardenRecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "GardenRecyclerAdapter";
+    Context context;
+    List<GardenAdapter> gardenHistory;
+    ImageLoader imageLoader;
+
+    public GardenRecyclerAdapter(List<GardenAdapter> getGardenAdapter, Context context){
+        super();
+        this.gardenHistory = getGardenAdapter;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder Viewholder, int position) {
+
+        GardenAdapter gardenAdapterOBJ =  gardenHistory.get(position);
+
+        imageLoader = ImageAdapter.getInstance(context).getImageLoader();
+
+        imageLoader.get(gardenAdapterOBJ.getImageUrl(),
+                ImageLoader.getImageListener(
+                        Viewholder.VollyImageView,//Server Image
+                        R.mipmap.ic_launcher,//Before loading server image the default showing image.
+                        android.R.drawable.ic_dialog_alert //Error image if requested image dose not found on server.
+                )
+        );
+
+        Viewholder.VollyImageView.setImageUrl(gardenAdapterOBJ.getImageUrl(), imageLoader);
+        Log.d(TAG, gardenAdapterOBJ.getImageUrl());
+        Viewholder.ImageTitleTextView.setText(gardenAdapterOBJ.getImageTitle());
+
+//
+//
+//
+//        Viewholder.itemLayout.setBackgroundColor(
+//                gardenAdapterOBJ.isThirsty() ?
+//                        Color.rgb(220, 120, 120) :
+//                        Color.rgb(46, 185, 113)
+//        );
+//
+//        if (gardenAdapterOBJ.isAutoWatering()){
+//            // mettre l'icone auto watering : ON
+//            int iconResource = context.getResources().getIdentifier(
+//                    "auto_on", "drawable", context.getPackageName() );
+//            Viewholder.autoIconView.setImageResource(iconResource);
+
+//        }
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return gardenHistory.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView ImageTitleTextView;
+        public NetworkImageView VollyImageView ;
+        public ImageView autoIconView ;
+        public RelativeLayout itemLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ImageTitleTextView = (TextView) itemView.findViewById(R.id.ImageNameTextView) ;
+
+            VollyImageView = (NetworkImageView) itemView.findViewById(R.id.VolleyImageView) ;
+
+//            autoIconView = itemView.findViewById(R.id.auto_watering_icon);
+//            itemLayout = itemView.findViewById(R.id.plantItemLayout);
+
+
+        }
+    }
+}
